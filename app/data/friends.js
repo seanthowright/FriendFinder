2dd7f907d56d8c2d64de814ee0b0f34a5e397fd3
Sean Wright <seanthowright@gmail.com>
2018-04-20 23:13:44 -0400

    Draft build, api route won't work

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+
+var friends = [
+    {
+        name: "Nathan",
+        photo: "https://vignette.wikia.nocookie.net/lookingforgroup/images/8/86/Placeholder_person.png/revision/latest?cb=20090224104512",
+        scores: [
+            "3",
+            "2",
+            "5",
+            "1",
+            "4",
+            "3",
+            "2",
+            "4",
+            "3",
+            "1"
+        ]
+    },
+    {
+        name: "Garrett",
+        photo: "https://vignette.wikia.nocookie.net/lookingforgroup/images/8/86/Placeholder_person.png/revision/latest?cb=20090224104512",
+        scores: [
+            "2",
+            "4",
+            "3",
+            "5",
+            "1",
+            "2",
+            "1",
+            "4",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Carl",
+        photo: "https://vignette.wikia.nocookie.net/lookingforgroup/images/8/86/Placeholder_person.png/revision/latest?cb=20090224104512",
+        scores: [
+            "4",
+            "4",
+            "3",
+            "2",
+            "2",
+            "2",
+            "1",
+            "5",
+            "4",
+            "3"
+        ]
+    },
+    {
+        name: "Cal",
+        photo: "",
+        scores: [
+            "5",
+            "5",
+            "4",
+            "4",
+            "3",
+            "3",
+            "2",
+            "2",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Kenneth",
+        photo: "https://vignette.wikia.nocookie.net/lookingforgroup/images/8/86/Placeholder_person.png/revision/latest?cb=20090224104512",
+        scores: [
+            "2",
+            "3",
+            "4",
+            "3",
+            "2",
+            "1",
+            "5",
+            "5",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Reece",
+        photo: "https://vignette.wikia.nocookie.net/lookingforgroup/images/8/86/Placeholder_person.png/revision/latest?cb=20090224104512",
+        scores: [
+            "1",
+            "1",
+            "2",
+            "2",
+            "5",
+            "4",
+            "5",
+            "4",
+            "2",
+            "3"
+        ]
+    }
+];
+
+module.exports = friends;
